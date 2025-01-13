f27ed756b61628000e6ba7326f8958f2d2ed10d4
Aravshreyas <advaysaim18@gmail.com>
2025-01-13 12:35:46 +0530

    Add Express server with /ping route for ASAP project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req, res) => {
+    res.send('Welcome to Space Quest')
+})
+
+app.get('/ping',(req,res)=>{
+    res.send('Pong!')
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
