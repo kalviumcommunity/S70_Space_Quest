c8a67741c22375b4820c4167714dc486d7e6bbd7
Aravshreyas <advaysaim18@gmail.com>
2025-02-03 14:20:18 +0530

    Added MongoDB connection and home route DB status

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const connectDatabase = require('./database'); 
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+
+
+// Connect to the database
+connectDatabase();
 
 app.get('/',(req, res) => {
-    res.send('Welcome to Space Quest')
+    const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected';
+    res.send({ message: "Welcome to ASAP Project", database: dbStatus } )  
 })
 
 app.get('/ping',(req,res)=>{
